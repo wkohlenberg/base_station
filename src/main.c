212c75db178e81d3328ae1e505629c6edaac1dc2
wkohlenberg <walterkohlenberg@gmail.com>
2019-11-07 22:20:09 +0100

    New: Initial commit.

---
+++ src/main.c
@@ -0,0 +1,17 @@
+// Bais Hello World
+//
+// Compile: gcc -Wall -lncurses -o main main.c
+// Run    : ./main
+
+#include <ncurses.h>
+
+int main()
+{
+	initscr();			// Start curses mode
+	printw("Hello World !!!");	// Print Hello World
+	refresh();			// Print it on to the real screen
+	getch();			// Wait for user input
+	endwin();			// End curses mode
+
+	return 0;
+}
