75d40c1b92f2423b834d343c7e30e4cf0032b89d
wkohlenberg <walterkohlenberg@gmail.com>
2019-11-10 22:10:20 +0100

    Add: class for sensor data windows. This class will control and update the data windows.

--- src/layout.cpp
+++ src/layout.cpp
@@ -2,38 +2,25 @@
 
 #include "layout.h"
 
+/**
+ * Class Layout
+ */
 CLayout::CLayout(){
 
 	// Print page title
 	pTitleWindow = newwin(1, COLS, 0, 0);
 	displayTitle(1);
 
-	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 1, 1));
-	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 1, 30));
-	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 1, 59));
-	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 13, 1));
-	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 13, 30));
-	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 13, 59));
-
-	for (unsigned i = 0; i < SENSOR_DATA_WINDOWS; i++){
-		wborder(m_winSensorData[i], '|', '|', '-', '-', '+', '+', '+', '+');
-		mvwprintw(m_winSensorData[i], 1, 1, "Sensor ...");
-		wrefresh(m_winSensorData[i]);
-	}
+	cSensorWindow.displaySensorDataWindows();
 
 	// Init footer
 	pFooterWindow = newwin(4, COLS, 26, 0);
-	box(pFooterWindow, 0, 0);
-	wrefresh(pFooterWindow);
+	displayFooter();
 }
 
 CLayout::~CLayout(){
 
-	for (unsigned i = 0; i < SENSOR_DATA_WINDOWS; i++){
-		delwin(m_winSensorData[i]);
-	}
 }
-
 
 void CLayout::displayTitle(unsigned page){
 	wclear(pTitleWindow);
@@ -58,4 +45,43 @@ void CLayout::displayTitle(unsigned page){
 
 	wattroff(pTitleWindow, COLOR_PAIR(1));
 	wrefresh(pTitleWindow);
+}
+
+void CLayout::displayFooter(){
+	box(pFooterWindow, 0, 0);
+	wrefresh(pFooterWindow);
+}
+
+int CLayout::getchar(){
+	return wgetch(pFooterWindow);
+}
+
+/**
+ * Class Layout
+ */
+CSensorWindow::CSensorWindow(){
+	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 1, 1));
+	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 1, 30));
+	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 1, 59));
+	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 13, 1));
+	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 13, 30));
+	m_winSensorData.push_back(newwin(SENSOR_DATA_WINDOW_HEIGHT, SENSOR_DATA_WINDOW_WIDTH, 13, 59));
+}
+
+CSensorWindow::~CSensorWindow(){
+	for (unsigned i = 0; i < SENSOR_DATA_WINDOWS; i++){
+		delwin(m_winSensorData[i]);
+	}
+}
+
+void CSensorWindow::updateSensorDataWindow(int nWin){
+		wborder(m_winSensorData[nWin], '|', '|', '-', '-', '+', '+', '+', '+');
+		mvwprintw(m_winSensorData[nWin], 1, 1, "Sensor ...");
+		wrefresh(m_winSensorData[nWin]);
+}
+
+void CSensorWindow::displaySensorDataWindows(){
+	for (unsigned i = 0; i < SENSOR_DATA_WINDOWS; i++){
+		updateSensorDataWindow(i);
+	}
 }
--- src/layout.h
+++ src/layout.h
@@ -10,14 +10,27 @@
 #define SENSOR_DATA_WINDOW_WIDTH	30
 #define SENSOR_DATA_WINDOW_HEIGHT	13
 
+class CSensorWindow{
+public:
+	CSensorWindow();
+	~CSensorWindow();
+
+	void updateSensorDataWindow(int nWin);
+	void displaySensorDataWindows();
+
+	std::vector<WINDOW*> m_winSensorData;
+};
+
 class CLayout{
 public:
 	CLayout();
 	~CLayout();
 
 	void displayTitle(unsigned page);
+	void displayFooter();
+	int getchar();
 
-	std::vector<WINDOW*> m_winSensorData;
+	CSensorWindow cSensorWindow;
 
 private:
 	WINDOW *pTitleWindow;
