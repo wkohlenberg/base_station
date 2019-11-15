6a1c2a1c4143d64033af872a3a0ec89b528c8535
wkohlenberg <walterkohlenberg@gmail.com>
2019-11-15 10:54:07 +0100

    New: Get data from serial communication and parse the data string.

---
+++ src/protocol.cpp
@@ -0,0 +1,67 @@
+#include <sstream>
+
+#include "protocol.h"
+#include "hello-xmega-lib.h"
+
+int ReceivePacket(char* aBuf, int nSize) {
+
+	uint8_t nPacket, nByte;
+	memset(aBuf, 0, nSize);				// Empty the buffer
+	while (!XmegaReadByte(&nPacket));	// Read the packet size (blocking style)
+
+	// Read each byte (blocking style)
+	for (uint i = 0; i < nPacket; i++) {
+		while (!XmegaReadByte(&nByte));
+		aBuf[i] = (char)nByte;
+	}
+
+	return (int)nPacket;
+}
+
+// Parse csv strings from serial communication
+std::vector<int> csvString(std::string dataString, char delim){
+
+	std::vector<int> result;
+	std::stringstream sstream(dataString);
+	while(sstream.good()){
+		std::string substr;
+		getline(sstream, substr, ',');
+		result.push_back(std::stoi(substr));
+	}
+
+	return result;
+}
+
+// Process serial communication and push data to the correct window
+int processSerialCommunication(CLayout layout){
+
+	uint8_t nType;
+	int nPacket = 0;
+	char aBuf[SERIAL_BUF_SIZE];
+
+	if (XmegaReadByte(&nType)) {
+
+		nPacket = ReceivePacket(aBuf, SERIAL_BUF_SIZE);
+		std::vector<int> data = csvString(aBuf, ',');
+		
+		switch (nType) {
+			case '!': // Type 1
+				//printf("! [%i] [%s]\n", nPacket, aBuf);
+				std::cout << "! [" << nPacket << "] ";
+				for (unsigned i = 0; i < data.size(); i++) {
+					std::cout << data.at(i) << " ";
+				}
+				std::cout << std::endl;
+				break;
+
+			case '@': // Type 2
+				printf("@ [%i] [%s]\n", nPacket, aBuf);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	return 0;
+}
---
+++ src/protocol.h
@@ -0,0 +1,15 @@
+#ifndef _PROTOCOL_H_
+#define _PROTOCOL_H_
+
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include "layout.h"
+
+
+#define SERIAL_BUF_SIZE			255
+
+int ReceivePacket(char* aBuf, int nSize);
+int processSerialCommunication(CLayout layout);
+
+#endif
